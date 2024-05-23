467436df82d5fb48cc29c5ae33698e4515aa6270
KrishJain256 <138436914+KrishJain256@users.noreply.github.com>
2024-05-23 23:00:10 +0530

    Update dbconnect.py to remove personal passwords

--- backend/mysql_helper/dbconnect.py
+++ backend/mysql_helper/dbconnect.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def estabilish_connection():
-    con = mysql.connector.connect(host="localhost", user="root", passwd="D1y_8U-leK")
+    con = mysql.connector.connect(host="localhost", user="root", passwd="[ENTER_YOUR_PASSWORD]")
     cursor = con.cursor()
     cursor.execute("create database attendance_app")
     con.commit()
